48e2e67cfc7d61dc63108d3857c634119e1ed4c7
gdavis-21 <gdavis19@uab.edu>
2022-07-20 14:39:11 -0500

    Initial Commit

---
+++ City-Sights/City_SightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SightsApp.swift
+//  City-Sights
+//
+//  Created by Grant Davis on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct City_SightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
